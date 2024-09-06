996ff48e7c35da6baa46755a000ddcfc5ff00195
ajjurathod <131947107+ajjurathod@users.noreply.github.com>
2024-09-06 13:14:19 +0530

    Create verify_service.sh

---
+++ verify_service.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Replace <PUBLIC_IP> with the actual public IP address of your EC2 instance
+PUBLIC_IP=47.129.175.128
+
+# Check if the application is running on port 8080 using the public IP address
+curl -Is http://47.129.175.128:8080 | head -n 1 | grep "200 OK" > /dev/null
+
+if [ $? -eq 0 ]; then
+  echo "Application is running successfully."
+else
+  echo "Application failed to start."
+  exit 1
+fi
+
